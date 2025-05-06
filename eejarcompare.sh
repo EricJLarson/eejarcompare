a691b0407a9bebd8641e5334068753264bfb3163
Eric Larson <elarson@akamai.com>
2025-05-06 18:43:02 -0400

    Remove many "&&"

--- eejarcompare.sh
+++ eejarcompare.sh
@@ -25,35 +25,36 @@ rm -r ${JAKARTADIR} 2>/dev/null || true; # May not yet exist
 rm -r ${JAVAXDIR} 2>/dev/null || true;  # May not yet exist
 mkdir ${JAKARTADIR};
 mkdir ${JAVAXDIR};
-export MIGRATIONTOOL=~/temp/jakartaee-migration-1.0.9/bin/migrate.sh;
 
-# Arg: Takes a fully qualified JAR name 
+# Arg: Takes a JAR file name and path relative to CWD.
 # Prints the JAR name prepended by "CHANGED:" if the JAR was changed, otherwise "SAME:"
 # Prereq:"fd" -- For parallelization, use "fd" instead of "find". 
-# Must be ran HIH/
+# Must be ran in HIH/
 # Calls to this method are not threadsafe because directories JAVAXDIR and JAKARTADIR are shared
 #    but this method itself is threaded by use of "fd".
 compare() {
   echo "${JAR}:" >&2;
   START=$(date +%s);
-  JARPATH=${1} &&\
-  JAR=$(basename ${JARPATH}) &&\
-  cp ${JARPATH} ${JAVAXDIR}/. &&\
+  JARPATH=${1}; 
+  JAR=$(basename ${JARPATH}); 
+  cp ${JARPATH} ${JAVAXDIR}/.; 
   ${MIGRATIONTOOL} \
     -profile=EE \
     ${JAVAXDIR}/${JAR} \
-    ${JAKARTADIR}/${JAR} &&\
-  \
-  pushd ${JAVAXDIR}/ > /dev/null &&\
-  jar -xf ${JAR} &&\
-  JAVAXMD5=$(fd -e class -x javap -c \; | sort | md5)  &&\
-  popd > /dev/null &&\
-  \
-  pushd ${JAKARTADIR}/ > /dev/null &&\
-  jar -xf ${JAR} &&\
-  JAKARTAMD5=$(fd -e class -x javap -c \; | sort | md5)  &&\
-  popd > /dev/null &&\
-  \
+    ${JAKARTADIR}/${JAR}; 
+  
+  pushd ${JAVAXDIR}/ > /dev/null; 
+  jar -xf ${JAR};
+  # We use "sort" to put the results of the two "fd" calls in form common to both.
+  # Because "fd" runs parallel jobs, the results arrive randomly. "fd | sort" is a map-reduce.   
+  JAVAXMD5=$(fd -e class -x javap -c \; | sort | md5);
+  popd > /dev/null;
+  
+  pushd ${JAKARTADIR}/ > /dev/null;
+  jar -xf ${JAR};
+  JAKARTAMD5=$(fd -e class -x javap -c \; | sort | md5);
+  popd > /dev/null; 
+  
   [ ${JAVAXMD5} == ${JAKARTAMD5} ] && echo "SAME:${JAR}:${JAVAXMD5:0:5}" || echo "CHANGED:${JAR}";
   rm -r ${JAKARTADIR}/${JAR};
   rm -r ${JAVAXDIR}/${JAR};
